15442d4119d679d2e52b23ece5f5bda8cb5ce4e3
Berchris Requiao <berchris.requiao@neueda.com>
2021-07-25 21:34:13 +0100

    Add Reactive Repository tests

---
+++ src/test/java/com/neueda/microservice/reactive/chassis/ReactiveMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.microservice.reactive.chassis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/neueda/microservice/reactive/chassis/repository/ChassisRepositoryTests.java
@@ -0,0 +1,60 @@
+package com.neueda.microservice.reactive.chassis.repository;
+
+
+import com.neueda.microservice.reactive.chassis.domain.ChassisEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.r2dbc.core.DatabaseClient;
+import reactor.core.publisher.Hooks;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+
+@SpringBootTest
+public class ChassisRepositoryTests {
+
+    @Autowired
+    private ChassisRepository chassisRepository;
+
+    @Autowired
+    private DatabaseClient database;
+
+    @BeforeEach
+    void setUp() {
+        Hooks.onOperatorDebug();
+
+        var statements = Arrays.asList(
+                "DROP TABLE IF EXISTS chassis_entity;",
+                "CREATE TABLE chassis_entity ( id IDENTITY PRIMARY KEY, name VARCHAR(50) NOT NULL, description VARCHAR(255));");
+
+        statements.forEach(it -> database.sql(it)
+                .fetch()
+                .rowsUpdated()
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete());
+    }
+
+    @Test
+    void readsAllEntitiesCorrectly() {
+        var chassisItem = new ChassisEntity();
+        chassisItem.setName("Chassis test");
+
+        insertChassisItem(chassisItem);
+
+        chassisRepository.findAll()
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    private void insertChassisItem(ChassisEntity... chassisEntities) {
+
+        chassisRepository.saveAll(Arrays.asList(chassisEntities))
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+}
