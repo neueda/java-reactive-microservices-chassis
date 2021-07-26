da2b8c8d7497a444a16f2317e0cb0c38901c748d
Berchris Requiao <berchris.requiao@neueda.com>
2021-07-26 17:08:34 +0100

    Add name parameter validation for whitespace

--- src/main/java/com/neueda/microservice/reactive/chassis/model/Chassis.java
+++ src/main/java/com/neueda/microservice/reactive/chassis/model/Chassis.java
@@ -3,6 +3,9 @@ package com.neueda.microservice.reactive.chassis.model;
 
 import javax.validation.constraints.NotBlank;
 
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
 import static org.springframework.util.StringUtils.hasText;
 
 public record Chassis(
@@ -10,7 +13,13 @@ public record Chassis(
         String name,
         String description) {
 
-    public Chassis { hasText(name); }
+    public Chassis {
+        requireNonNull(name);
+        if (name.isBlank())
+            throw new IllegalArgumentException("Name cannot be blank");
+    }
 
-    public Chassis(String name) { this(name, null); }
+    public Chassis(String name) {
+        this(name, null);
+    }
 }
