e8dffca5f6d42570e4022a92ad547a602c4b0fba
Berchris Requiao <berchris.requiao@neueda.com>
2021-07-27 02:38:13 +0100

    Remove blockhound dependency
    Removed due to issues with OpenJDK version 13+
    https://github.com/reactor/BlockHound/issues/33

--- src/main/java/com/neueda/microservice/reactive/chassis/ReactiveMicroserviceApplication.java
+++ src/main/java/com/neueda/microservice/reactive/chassis/ReactiveMicroserviceApplication.java
@@ -3,14 +3,12 @@ package com.neueda.microservice.reactive.chassis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class ReactiveMicroserviceApplication {
 
 	public static void main(String[] args) {
-		BlockHound.install();
 		SpringApplication.run(ReactiveMicroserviceApplication.class, args);
 	}
 
