3dd3df1bfb9a8546f36124dc2f4fec78aef2b82b
Berchris Requiao <berchris.requiao@neueda.com>
2021-07-28 15:54:13 +0100

    Add retry to webclient call

--- src/main/java/com/neueda/microservice/reactive/chassis/client/ClientHelper.java
+++ src/main/java/com/neueda/microservice/reactive/chassis/client/ClientHelper.java
@@ -3,18 +3,21 @@ package com.neueda.microservice.reactive.chassis.client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 import java.net.URI;
+import java.time.Duration;
 
 @RequiredArgsConstructor
 class ClientHelper {
 
     private final WebClient webClient;
 
-    <T> Mono<T> performGetRequest(URI uri, Class<T> returnType) {
+    <T> Mono<T> performGetRequest(URI uri, Class<T> clazz) {
         return webClient.get()
                 .uri(uri)
                 .retrieve()
-                .bodyToMono(returnType);
+                .bodyToMono(clazz)
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(3)));
     }
 }
