ba01ac48a0922aa78baf46726c4e286c26bc6334
Berchris Requiao <berchris.requiao@neueda.com>
2021-07-31 21:15:38 +0100

    Break down parameter validation responsibilities

--- src/main/java/com/neueda/microservice/reactive/chassis/client/GitHubClient.java
+++ src/main/java/com/neueda/microservice/reactive/chassis/client/GitHubClient.java
@@ -1,11 +1,14 @@
 package com.neueda.microservice.reactive.chassis.client;
 
+import com.neueda.microservice.reactive.chassis.exception.MandatoryPathParameterException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
 
+import static java.lang.String.format;
 import static org.springframework.web.util.UriComponentsBuilder.fromUri;
 
 @Service
@@ -23,11 +26,16 @@ public class GitHubClient {
     }
 
     public Mono<String> searchUser(String username) {
-        URI uri = fromUri(baseUri).path("/users")
+        if (!StringUtils.hasText(username)) {
+            throw new MandatoryPathParameterException("chassisClient/{username}",
+                    format("'username' have to have length greater than 0, " +
+                            "and contains at least one non-whitespace character. Current value: [%s]", username));
+        }
+
+        return clientHelper.performGetRequest(fromUri(baseUri)
+                .path("/users")
                 .queryParam("q", username + "+repos:>0")
                 .build()
-                .toUri();
-
-        return clientHelper.performGetRequest(uri, String.class);
+                .toUri(), String.class);
     }
 }
--- src/main/java/com/neueda/microservice/reactive/chassis/controller/ChassisController.java
+++ src/main/java/com/neueda/microservice/reactive/chassis/controller/ChassisController.java
@@ -7,12 +7,19 @@ import com.neueda.microservice.reactive.chassis.model.Chassis;
 import com.neueda.microservice.reactive.chassis.service.ChassisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1")
@@ -21,6 +28,7 @@ public class ChassisController {
 
     private final ChassisService chassisService;
     private final GitHubClient gitHubClient;
+
 
     @GetMapping("chassis")
     public Flux<Chassis> getAllChassis() {
@@ -42,11 +50,11 @@ public class ChassisController {
     }
 
     @GetMapping({"chassisClient", "chassisClient/{username}"})
-    public Mono<String> getChassisClientResponse(@PathVariable(required = false) String username) {
-        if (StringUtils.hasText(username))
-            return gitHubClient.searchUser(username);
+    public Mono<String> getChassisClientResponse(@PathVariable Optional<String> username) {
+        String value = username.orElseThrow(() ->
+                new MandatoryPathParameterException("chassisClient/{username}", "'username' is mandatory"));
 
-        throw new MandatoryPathParameterException("chassisClient/{username}", "'username' is mandatory. Value: [" + username + "]");
+        return gitHubClient.searchUser(value);
     }
 
     @PostMapping("chassis")
