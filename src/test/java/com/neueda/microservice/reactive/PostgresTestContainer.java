16bfe340547e7022821676d306652fc6e6712d57
Berchris Requiao <berchris.requiao@neueda.com>
2021-08-04 14:01:17 +0100

    Expose testcontainers log

--- src/test/java/com/neueda/microservice/reactive/PostgresTestContainer.java
+++ src/test/java/com/neueda/microservice/reactive/PostgresTestContainer.java
@@ -1,18 +1,21 @@
 package com.neueda.microservice.reactive;
 
 import io.r2dbc.spi.ConnectionFactories;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.r2dbc.connection.init.ScriptUtils;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.PostgreSQLR2DBCDatabaseContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.utility.DockerImageName;
 import reactor.core.publisher.Mono;
 
 import static java.lang.String.format;
 import static java.util.Collections.singletonMap;
 
+@Slf4j
 public abstract class PostgresTestContainer {
     
     private static final DockerImageName postgresImage =
@@ -26,6 +29,8 @@ public abstract class PostgresTestContainer {
     // https://github.com/testcontainers/testcontainers-java/issues/2352
     static {
         postgresContainer.start();
+        postgresContainer.followOutput(
+                new Slf4jLogConsumer(log).withSeparateOutputStreams());
     }
 
     @DynamicPropertySource
