dfe7006ce77051659d3110956efb71ef33191a59
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2021-08-09 17:18:10 +0100

    Add ReactiveSortingRepository extension

--- src/main/java/com/neueda/microservice/reactive/repository/ChassisRepository.java
+++ src/main/java/com/neueda/microservice/reactive/repository/ChassisRepository.java
@@ -2,9 +2,9 @@ package com.neueda.microservice.reactive.repository;
 
 import com.neueda.microservice.reactive.entity.ChassisEntity;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 
 public interface ChassisRepository extends
-        ReactiveCrudRepository<ChassisEntity, Long>,
+        ReactiveSortingRepository<ChassisEntity, Long>,
         ReactiveQueryByExampleExecutor<ChassisEntity> {
 }
