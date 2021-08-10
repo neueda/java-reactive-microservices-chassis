99b9333fdd77c76683bf365d561dbfc01fd0e202
Berchris Requiao <berchris.requiao@neueda.com>
2021-08-10 11:56:10 +0100

    Remove lombok @Data annotation

--- src/main/java/com/neueda/microservice/reactive/entity/ChassisEntity.java
+++ src/main/java/com/neueda/microservice/reactive/entity/ChassisEntity.java
@@ -2,11 +2,13 @@ package com.neueda.microservice.reactive.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
