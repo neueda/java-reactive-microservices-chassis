fbdadd55e72644a811d4d8f00d26259ddd8f09e4
Berchris Requiao <berchris.requiao@neueda.com>
2021-08-11 10:51:31 +0100

    Change the access level of the entity all args constructor

--- src/main/java/com/neueda/microservice/reactive/entity/ChassisEntity.java
+++ src/main/java/com/neueda/microservice/reactive/entity/ChassisEntity.java
@@ -7,12 +7,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
+import static lombok.AccessLevel.PRIVATE;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(access = PRIVATE)
 public class ChassisEntity {
+
     @Id
     private Long id;
     private String name;
