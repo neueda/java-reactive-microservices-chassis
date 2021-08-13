8cccf3cbbddf652331bf7018baf4934aeec7be5f
Berchris Requiao <berchris.requiao@neueda.com>
2021-08-13 12:05:15 +0100

    Add contract label

--- src/test/resources/contracts/chassis/should_list_all_chassis.groovy
+++ src/test/resources/contracts/chassis/should_list_all_chassis.groovy
@@ -3,6 +3,7 @@ package contracts.chassis
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
+    label "list_all_chassis"
     description "should return a list of all chassis items"
 
     request {
