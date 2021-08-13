dae09b6d47989bab7b2176ee5535ef0e504d021d
Berchris Requiao <berchris.requiao@neueda.com>
2021-08-13 12:35:09 +0100

    Remove label
    Label is only required to be used with Messaging frameworks.

--- src/test/resources/contracts/chassis/should_list_all_chassis.groovy
+++ src/test/resources/contracts/chassis/should_list_all_chassis.groovy
@@ -3,7 +3,6 @@ package contracts.chassis
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
-    label "list_all_chassis"
     description "should return a list of all chassis items"
 
     request {
