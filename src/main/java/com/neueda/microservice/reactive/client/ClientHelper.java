7ac107e38bb20985e3d74ae39aed8666e7d0a0c4
Berchris Requiao <berchris.requiao@neueda.com>
2021-08-16 10:22:21 +0100

    Refactor ClientHelper class

--- src/main/java/com/neueda/microservice/reactive/client/ClientHelper.java
+++ src/main/java/com/neueda/microservice/reactive/client/ClientHelper.java
@@ -1,9 +1,13 @@
 package com.neueda.microservice.reactive.client;
 
-import lombok.RequiredArgsConstructor;
+import com.neueda.microservice.reactive.properties.ClientProperties;
+import io.netty.handler.logging.LogLevel;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriBuilder;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 import java.net.URI;
 import java.time.Duration;
@@ -13,10 +17,28 @@ import static org.springframework.util.StringUtils.hasText;
 import static org.springframework.util.StringUtils.trimWhitespace;
 import static reactor.util.retry.Retry.backoff;
 
-@RequiredArgsConstructor
 class ClientHelper {
 
     private final WebClient webClient;
+
+    ClientHelper(WebClient.Builder webClientBuilder, ClientProperties props, String logLevel) {
+        if (isLogEnabled(logLevel)) {
+            webClientBuilder.clientConnector(
+                    new ReactorClientHttpConnector(HttpClient.create().wiretap(
+                            "reactor.netty.http.client.HttpClient",
+                            LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL)));
+        }
+
+        this.webClient = webClientBuilder
+                .baseUrl(props.baseUrl().toString())
+                .build();
+    }
+
+    private boolean isLogEnabled(String logLevel) {
+        logLevel = trimWhitespace(logLevel);
+        return hasText(logLevel) &&
+                (logLevel.equalsIgnoreCase("debug") || logLevel.equalsIgnoreCase("trace"));
+    }
 
     <T> Mono<T> performGetRequest(Function<UriBuilder, URI> uriFunction, Class<T> clazz) {
         return webClient.get()
@@ -24,11 +46,5 @@ class ClientHelper {
                 .retrieve()
                 .bodyToMono(clazz)
                 .retryWhen(backoff(3, Duration.ofSeconds(3)));
-    }
-
-    static boolean isLogEnabled(String logLevel) {
-        logLevel = trimWhitespace(logLevel);
-        return hasText(logLevel) &&
-                (logLevel.equalsIgnoreCase("debug") || logLevel.equalsIgnoreCase("trace"));
     }
 }
--- src/main/java/com/neueda/microservice/reactive/client/GitHubClient.java
+++ src/main/java/com/neueda/microservice/reactive/client/GitHubClient.java
@@ -1,19 +1,11 @@
 package com.neueda.microservice.reactive.client;
 
 import com.neueda.microservice.reactive.properties.ClientProperties;
-import io.netty.handler.logging.LogLevel;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.transport.logging.AdvancedByteBufFormat;
-
-import static org.springframework.util.StringUtils.hasText;
-import static org.springframework.util.StringUtils.trimWhitespace;
 
 @Service
 public class GitHubClient {
@@ -24,17 +16,7 @@ public class GitHubClient {
     public GitHubClient(WebClient.Builder webClientBuilder, ClientProperties props,
                         @Value("${logging.level.reactor.netty.http.client:}") String logLevel) {
 
-        if (ClientHelper.isLogEnabled(logLevel)) {
-            webClientBuilder.clientConnector(
-                    new ReactorClientHttpConnector(HttpClient.create().wiretap(
-                            "reactor.netty.http.client.HttpClient",
-                            LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL)));
-        }
-
-        this.clientHelper =
-                new ClientHelper(webClientBuilder
-                        .baseUrl(props.baseUrl().toString())
-                        .build());
+        this.clientHelper = new ClientHelper(webClientBuilder, props, logLevel);
     }
 
     public Mono<String> searchUsernameContaining(@NonNull String value) {
