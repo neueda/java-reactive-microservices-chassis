5aeb342f315594b6455421fea6baad38d1118162
Berchris Requiao <berchris.requiao@neueda.com>
2021-08-25 15:44:33 +0100

    Remove leftover imports

--- src/main/java/com/neueda/microservice/reactive/configuration/OpenApiConfig.java
+++ src/main/java/com/neueda/microservice/reactive/configuration/OpenApiConfig.java
@@ -3,7 +3,6 @@ package com.neueda.microservice.reactive.configuration;
 import com.neueda.microservice.reactive.properties.ApiInfoProperties;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
